84b6b77ae6222d420d942106188dd8b6bc007781
Kalyan Mazumder <kalyaninform@gmail.com>
2021-01-18 15:55:46 +0530

    refactor:dev: controller to blade view

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -9,12 +9,18 @@ class CustomerController extends Controller
     //
     function submit(){
     //   var_dump(request('first-name'));
-        echo (request('first-name'));
-        echo (request('last-name'));
-        echo (request('mobile-number'));
-        echo (request('email'));
+//        echo (request('first-name'));
+//        echo (request('last-name'));
+//        echo (request('mobile-number'));
+//        echo (request('email'));
         // return view('customer',['collection'=>$data['data']]);
-
+        $data = array(
+            'firstName'=>request('first-name'),
+            'lastName'=>request('last-name'),
+            'mobileNumber'=>request('mobile-number'),
+            'email'=>request('email')
+        );
+        return view('tabledata')->with($data);
     }
 
 }
--- resources/views/tabledata.blade.php
+++ resources/views/tabledata.blade.php
@@ -1,19 +1,19 @@
 <table border="1">
     <tr>
-        <td>First Nmae</td>
+        <td>First Name</td>
         <td>Last Name</td>
         <td>Mobile Number</td>
         <td>Email</td>
-        
+
     </tr>
-    @foreach ($collection as $customer)
+    @foreach ($data as $customer)
         <tr>
-        <td>{{$submit['first-name']}}</td>
-        <td>{{$submit['last-name']}}</td>
-        <td>{{$submit['mobile-number']}}</td>
-        <td>{{$submit['email']}}</td>
-            
+        <td>{{$customer->firstName}}</td>
+        <td>{{$customer->lastName}}</td>
+        <td>{{$customer->mobileNumber}}</td>
+        <td>{{$customer->email}}</td>
+
         </tr>
     @endforeach
-   
+
 </table>
